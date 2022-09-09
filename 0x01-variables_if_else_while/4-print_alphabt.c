55c9d07a9972fbd226ee1c8f902c4c3e365b3592
Deju12 <dejutesfa12@gmail.com>
2022-09-09 05:02:04 -0700

    fix betty

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,7 +7,8 @@
 int main(void)
 {
 	char c = 'a';
-while (c <= 'z')
+
+	while (c <= 'z')
 	{
 		if (c != 'e' && c != 'q')
 		{
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,7 +10,8 @@
 int main(void)
 {
 	char c = '0';
-while (c <= '9')
+
+	while (c <= '9')
 	{
 		putchar(c);
 		c++;
